2cc102ee4eb8411e7f21fc8afae45b315b53a855
Sherzod Abdurahmanov <sherzodrts@gmaill.com>
2025-03-23 11:36:59 +0300

    start project

---
+++ handlers/default_handlers/start.py
@@ -0,0 +1,9 @@
+from telebot.types import Message
+from loader import bot
+
+
+
+@bot.message_handler(commands=["start"])
+def bot_start(message: Message):
+    bot.send_message(message.from_user.id,
+                     f'Привет ✋{message.from_user.full_name}\nЯ бот 🤖')
